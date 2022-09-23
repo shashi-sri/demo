221f713de02976d1fe2843eb797730f1465e9c3b
Jukanti ShashiSri <jshashisri@nisum.com>
2022-09-23 17:04:35 +0530

    two files added

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee{
+ private int empid;
+private string ename;
+private double salary;
+public Employee(){}
+public Employee(int empid,String ename,double salary)
+{
+	this.empid=empid;
+}
+public static void main(){
+}
+}
